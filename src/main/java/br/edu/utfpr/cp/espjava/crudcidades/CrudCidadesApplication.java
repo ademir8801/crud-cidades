5176f276b1a3586b6c05f354a32ab858ca16b527
Ademir Martins <ademir.m.junior@gmail.com>
2021-04-27 19:54:59 -0300

    Validando o método criar()

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/CrudCidadesApplication.java
@@ -1,11 +1,12 @@
 package br.edu.utfpr.cp.espjava.crudcidades;
 
+import javax.validation.Validator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
