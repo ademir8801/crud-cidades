8ed56a0bfb81e53f3e61742bb6f4d7fdb809629c
Ademir Martins <ademir.m.junior@gmail.com>
2021-05-13 18:39:30 -0300

    Definidas permissoes dos usuarios no metodo configure(HttpSecurity http)

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.edu.utfpr.cp.espjava.crudcidades;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,6 +23,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                     .withUser("anna")
                     .password(cifrador().encode("test123"))
                     .authorities("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/").hasAnyAuthority("listar", "admin")
+            .antMatchers("/criar").hasAuthority("admin")
+            .antMatchers("/excluir").hasAuthority("admin")
+            .antMatchers("/preparaAlterar").hasAuthority("admin")
+            .antMatchers("/alterar").hasAuthority("admin")
+            .anyRequest().denyAll()
+                .and()
+            .formLogin().permitAll();
     }
 
     @Bean
