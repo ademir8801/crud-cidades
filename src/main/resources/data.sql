da6df09b7044eb610ec9b25a853c0ce91c123891
Ademir Martins <ademir.m.junior@gmail.com>
2021-05-14 11:52:13 -0300

    Definido o metodo para capturar a senha cifrada e criado o arquivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `usuario` VALUES (1, 'john', '$2a$10$Hsqy7psC2idmpA0h.JI.9.HrPKoXtU7v32c0Q1Dj3622nDe5cLX96');
+INSERT INTO `usuario` VALUES (2, 'anna', '$2a$10$Hsqy7psC2idmpA0h.JI.9.HrPKoXtU7v32c0Q1Dj3622nDe5cLX96');
+
+INSERT INTO `usuario_papeis` VALUES (1, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'listar');
+INSERT INTO `usuario_papeis` VALUES (2, 'admin');
